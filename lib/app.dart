a33f29e13920be265645ed8ad83efbff5bb8eeff
samadurm <samadurm@oregonstate.edu>
2020-08-07 17:22:04 -0700

    Added list screen

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,14 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
+import 'package:wasteagram/screens/list_screen.dart';
 
 class App extends StatelessWidget {
+  
+  static final routes = {
+    ListScreen.routeName : (context) => ListScreen(),
+  };
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
---
+++ lib/screens/list_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ListScreen extends StatefulWidget {
+  
+  static const routeName = '/';
+
+  @override
+  _ListScreenState createState() => _ListScreenState();
+}
+
+class _ListScreenState extends State<ListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
