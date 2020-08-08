9837e033c7d2ead8f9a84332cdfa8af3f67a2a22
samadurm <samadurm@oregonstate.edu>
2020-08-07 18:16:37 -0700

    Changed latitude and longitude from string to double to better represent data type

--- lib/models/post.dart
+++ lib/models/post.dart
@@ -3,8 +3,8 @@ class Post{
   final int wastedItems;
   final String imageURL;
   final DateTime date;
-  final String latitude;
-  final String longitude;
+  final double latitude;
+  final double longitude;
 
   Post({this.wastedItems, this.imageURL, this.date, this.latitude, this.longitude});
 
