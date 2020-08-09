5353ddcfb67322010e4b4b6f9ddc3d244baa178b
samadurm <samadurm@oregonstate.edu>
2020-08-08 17:53:22 -0700

    can now select an image when adding a new post, which populates on the New Post screen

--- lib/screens/new_post_screen.dart
+++ lib/screens/new_post_screen.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:wasteagram/widgets/main_scaffold.dart';
 
 class NewPostScreen extends StatefulWidget {
@@ -10,11 +12,34 @@ class NewPostScreen extends StatefulWidget {
 }
 
 class _NewPostScreenState extends State<NewPostScreen> {
+  File image;
+
+  @override
+  void initState(){
+    super.initState();
+    getImage();
+  }
+
+  Future getImage() async {
+    final pickedImage = await ImagePicker().getImage(source: ImageSource.camera);
+    setState(() {
+      image = File(pickedImage.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MainScaffold(
       title: Text('Wastegram'),
-      body: Center(child: Text('Reached the new post screen'),),
+      body: Center(child: image == null ? CircularProgressIndicator() : sizedImage(image),),
     );
   }
+}
+
+SizedBox sizedImage(File imageFile){
+  return SizedBox(
+    height: 300,
+    width: 400,
+    child: Image.file(imageFile),
+  );
 }
