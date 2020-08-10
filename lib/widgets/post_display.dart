7b3d2e54d3e0b8cb98d9f53b671cc1d3d73f5e74
samadurm <samadurm@oregonstate.edu>
2020-08-09 17:44:00 -0700

    List Screen retrieves mock data from firebase

--- lib/widgets/post_display.dart
+++ lib/widgets/post_display.dart
@@ -1,26 +1,60 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:wasteagram/models/post.dart';
 import 'package:wasteagram/screens/detail_screen.dart';
 
 
-class PostDisplay extends StatelessWidget {
+// class PostDisplay extends StatelessWidget {
+  
+//   final List<Post> posts;
+
+//   PostDisplay({this.posts});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return ListView.builder(
+//       itemCount: posts.length,
+//       itemBuilder: (context, index) {
+//         return ListTile(
+//           title: Text('${DateFormat('EEEE, MMM d, yyyy').format(posts[index].date)}'),
+//           trailing: Text('${posts[index].wastedItems}', textScaleFactor: 1.3,),
+//           onTap: () => Navigator.pushNamed(context, DetailScreen.routeName, arguments: posts[index])
+//         );
+//       }
+//     );
+//   }
+// }
+
+class PostDisplay extends StatefulWidget {
   
   final List<Post> posts;
 
   PostDisplay({this.posts});
+  
+  @override
+  _PostDisplayState createState() => _PostDisplayState();
+}
 
+class _PostDisplayState extends State<PostDisplay> {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: posts.length,
-      itemBuilder: (context, index) {
-        return ListTile(
-          title: Text('${DateFormat('EEEE, MMM d, yyyy').format(posts[index].date)}'),
-          trailing: Text('${posts[index].wastedItems}', textScaleFactor: 1.3,),
-          onTap: () => Navigator.pushNamed(context, DetailScreen.routeName, arguments: posts[index])
+    return StreamBuilder(
+      stream: Firestore.instance.collection('mock-posts').snapshots(),
+      builder: (context, snapshot){
+        return ListView.builder(
+          itemCount: snapshot.data.documents.length,
+          itemBuilder: (context, index){
+            var post = snapshot.data.documents[index];
+            return ListTile(
+              // ${DateFormat('EEEE, MMM d, yyyy').format(posts[index].date)
+              title: Text(post['date'].toString()),
+              trailing: Text(post['wastedItems'].toString(), textScaleFactor: 1.3,),
+              // onTap: () => Navigator.pushNamed(context, DetailScreen.routeName, arguments: posts[index])
+            );
+          }
         );
-      }
+      },
     );
   }
 }
